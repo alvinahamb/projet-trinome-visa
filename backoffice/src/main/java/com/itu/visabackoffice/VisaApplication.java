d421fad96973311bdfbff949955758ba86417647
razherana <razherana@gmail.com>
2026-04-18 04:09:20 +0300

    fix: Fixed springboot backoffice to start and work with database

--- backoffice/src/main/java/com/itu/visabackoffice/VisaApplication.java
+++ backoffice/src/main/java/com/itu/visabackoffice/VisaApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VisaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VisaApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(VisaApplication.class, args);
+  }
 }
